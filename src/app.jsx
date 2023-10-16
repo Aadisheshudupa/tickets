38a5f2d5ab713c99b1749b618c33e035417e8a13
Aadisheshudupa <116788957+Aadisheshudupa@users.noreply.github.com>
2023-10-16 14:27:48 +0530

    Add files via upload

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Header from "./header";
+function App()
+{
+    return <div>
+        <Header/>
+    </div>;
+}
+export default App;
---
+++ src/header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css"
+import logo from "./logo.png"
+
+function Header()
+{
+    return <header>
+    <img src={logo} alt="" / >
+    <input type="search" name="" id=""/>
+    </header>;
+}
+export default Header;
