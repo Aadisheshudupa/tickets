6a0d38be835cc259b9727f75b6b7cbaced5740e2
Aadisheshudupa <aadiudu@gmail.com>
2023-11-20 20:28:54 +0530

    change in domain

--- src/seatBooking.jsx
+++ src/seatBooking.jsx
@@ -53,7 +53,7 @@ today2.setDate(today1.getDate()+1);
   
   const fetchData = async () => {
     try {
-      const response = await fetch('http://localhost:3002/api/data');
+      const response = await fetch('https://tickets-ebon.vercel.app//api/data');
       const result = await response.json();
       console.log(result);
       setresult(result);
@@ -131,8 +131,9 @@ today2.setDate(today1.getDate()+1);
       
     
       try {
-        const response = await fetch('http://localhost:3002/api/data', {
+        const response = await fetch('https://tickets-ebon.vercel.app//api/data', {
           method: 'POST',
+          mode:'cors',
           headers: {
             'Content-Type': 'application/json',
           },
